d6d0cf8b2ef21d7b72b7e1ff087c511d8d65c384
liu-wb <136907108@qq.com>
2022-07-06 18:00:31 +0800

    杂项配置

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,16 @@
+// main
 import { createApp } from 'vue'
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+// css
+import '@/style/index.less'
+
+// components
+
+const app = createApp(App)
+app.use(router)
+    .use(ArcoVue)
+app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+const resolve = (dirname) => path.join(__dirname, dirname)
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  /**
+   * 设置全局less变量文件
+   */
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [path.resolve(__dirname, 'src/style/variable.less')]
+    }
+  }
 })
